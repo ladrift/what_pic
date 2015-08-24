9c0c8619527263c88e294b1164058ade2553a828
ladrift <cxfyzy@gmail.com>
2015-08-24 14:45:12 +0800

    no action form
    using javascript to handle image display

--- what_pic/main/views.py
+++ what_pic/main/views.py
@@ -1,18 +1,7 @@
 from flask import render_template, session, url_for, current_app, redirect
 from . import main
 from .forms import ImageUrlForm
-from ..cloud_sight import cloud_sight
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/')
 def index():
-    form = ImageUrlForm()
-    if form.validate_on_submit():
-        result = cloud_sight(form.url.data)
-        if result:
-            session['url'] = form.url.data
-            session['result'] = result
-        return redirect(url_for('.index'))
-
-    return render_template('index.html',
-                          form=form, result=session.get('result'),
-                          url=session.get('url'))
+    return render_template('index.html')
